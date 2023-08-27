74db1d0b0d6aa4014dfc938c14e4ae49376a95f9
nopa12 <28837513+nopa12@users.noreply.github.com>
2023-08-27 12:23:02 +0300

    build wix installer

---
+++ build.ps1
@@ -0,0 +1,39 @@
+# get wix toolset
+$zipUrl = "https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311-binaries.zip"
+$zipFilePath = "$PWD\wix311-binaries.zip"
+$extractPath = "$PWD\wix311-binaries"
+
+if (Test-Path -Path $extractPath) {
+    Write-Host "wix311-binaries Directory already exists. Skipping download."
+} else {
+    Write-Host "Downloading wix toolset wix311-binaries..."
+    Invoke-WebRequest -Uri $zipUrl -OutFile $zipFilePath
+    
+    Write-Host "Unpacking ZIP file..."
+    Expand-Archive -Path $zipFilePath -DestinationPath $extractPath
+}
+
+# build custom action
+cd c-custom-action
+Start-Process -NoNewWindow -FilePath ".\build_test.cmd"
+cd ..\
+
+
+# build go-dll
+cd go-dll
+& ".\build.ps1"
+cd ..\
+
+# Build object
+$candleParams = "-nologo -v "`
+                + "-out build\ "`
+                + "wix-installer\Product.wxs "`
+
+Start-Process -NoNewWindow -FilePath wix311-binaries\candle.exe -ArgumentList $candleParams -Wait
+
+# Create MSI
+$lightParams =  "-nologo -v "`
+                + '-out "build\\installer.msi" '`
+                + 'build\Product.wixobj '`
+
+Start-Process -NoNewWindow -FilePath wix311-binaries\light.exe -ArgumentList $lightParams -Wait
